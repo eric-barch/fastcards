bebe2a982697f22114a86e2bf4bdacb7c0a7693e
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2023-09-18 16:25:41 -0400

    adjust note __repr__ method

--- models/text/notes/note.py
+++ models/text/notes/note.py
@@ -4,21 +4,25 @@ class Note:
         self.source = source
         self.front = token["front"]
         self.back = token["back"]
+        self.pos = token["pos"]
+        self.gender = token["gender"]
+        self.number = token["number"]
         self.representation = token["representation"]
         self.start = token["start"]
         self.end = token["end"]
         self.lemma = token["lemma"]
-        self.pos = token["pos"]
-        self.gender = token["gender"]
-        self.number = token["number"]
         self.already_exists = self.check_if_already_exists()
 
     def __repr__(self):
         front = f"front: {self.front}"
         back = f"back: {self.back}"
+        pos = f"pos: {self.pos}"
+        gender = f"gender: {self.gender}"
+        number = f"number: {self.number}"
         already_exists = f"already_exists: {self.already_exists}"
+        spacer = f"\n{'':<5}"
 
-        return f"{front:<30}{back:<30}{already_exists}"
+        return f"{front}{spacer}{back}{spacer}{pos}{spacer}{gender}{spacer}{number}{spacer}{already_exists}"
 
     def check_if_already_exists(self):
         existing_notes = self.session.anki.find_notes_by_front(
