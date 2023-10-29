781e8a0ac820ba38d8189e1aeb5b7cd64dcf24e3
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2023-10-29 12:31:09 -0400

    refactor Text to Tokens class

--- interfaces/user_interface.py
+++ interfaces/user_interface.py
@@ -1,37 +1,35 @@
 class UserInterface:
-    def __init__(self, session):
-        self.session = session
+    def __init__(self):
+        pass
 
-    def select_deck_names(self):
-        read_deck_name = self.request_deck_name(
-            "Choose a deck to read for existing notes: "
+    def select_decks(self, available_decks):
+        read_deck_name = self.select_deck(
+            "Choose a deck to read for existing notes: ", available_decks
         )
         print(f"\nRead deck selected: {read_deck_name}")
 
-        write_deck_name = self.request_deck_name(
-            "Choose a deck to write new notes to: "
+        write_deck = self.select_deck(
+            "Choose a deck to write new notes to: ", available_decks
         )
-        print(f"\nWrite deck selected: {write_deck_name}")
+        print(f"\nWrite deck selected: {write_deck}")
 
-        return read_deck_name, write_deck_name
+        return read_deck_name, write_deck
 
-    def request_deck_name(self, prompt):
-        all_deck_names = self.session.anki.get_all_deck_names()
-
+    def select_deck(self, prompt, available_decks):
         print("\nAll decks:\n")
 
-        for i, deck_name in enumerate(all_deck_names, start=1):
+        for i, deck_name in enumerate(available_decks, start=1):
             print(f"{i}. {deck_name}")
 
         while True:
             user_input = input(f"\n{prompt}")
 
-            result = self.validate_deck_name_input(user_input, len(all_deck_names))
+            result = self.validate_deck_selection(user_input, len(available_decks))
 
             if result:
-                return all_deck_names[result - 1]
+                return available_decks[result - 1]
 
-    def validate_deck_name_input(self, user_input, max_value):
+    def validate_deck_selection(self, user_input, max_value):
         try:
             choice = int(user_input)
         except:
@@ -44,58 +42,8 @@ class UserInterface:
 
         return choice
 
-    def request_input(self):
+    def enter_input(self):
         user_input = input(
             "\nEnter a string in the source language ('restart' to change decks, 'exit' to quit):\n\n"
         )
         return user_input
-
-    def select_tokens(self):
-        tokens = self.session.text.tokens
-
-        print(f"\nExtracted tokens:\n{tokens}")
-
-        while True:
-            user_input = input(
-                "\nEnter the number(s) of the token(s) you want to create notes for, separated by "
-                "commas or type 'a' for all (will skip duplicates and proper nouns):\n\n"
-            )
-
-            result = self.validate_new_tokens_input(user_input, len(tokens))
-
-            if result:
-                token_indices = []
-
-                if result == "a":
-                    for i, token in enumerate(tokens):
-                        token_indices.append(i)
-                else:
-                    for token_number in result:
-                        token_index = token_number - 1
-                        token_indices.append(token_index)
-
-                return token_indices
-
-    def validate_new_tokens_input(self, user_input, max_value):
-        if user_input.lower().strip() == "a":
-            return "a"
-
-        try:
-            user_input_indices = [int(i) for i in user_input.split(",")]
-
-            # Check if the indices are within the valid range
-            if any(i < 1 or i > max_value for i in user_input_indices):
-                print(
-                    f"\nInvalid. One or more of your choices falls outside the range of "
-                    f"Tokens (1-{max_value})."
-                )
-                return None
-
-            return user_input_indices
-
-        except ValueError:
-            print(
-                "\nPlease enter 'a' or a comma-separated list of integers specifying the new tokens "
-                "you want to create."
-            )
-            return None
--- models/tokens.py
+++ models/tokens.py
@@ -1,109 +1,31 @@
 import spacy
 
-from .token import Token
-from .spacy_token import SpacyToken
-from .openai_token import OpenAiToken
+from models.token import Token
 
 nlp = spacy.load("fr_dep_news_trf")
 
 
 class Tokens(list):
-    def __init__(self, session, text):
+    def __init__(self, input):
         super().__init__()
-        self.session = session
-        self.text = text
+        self.extend(self.tokenize(input))
 
-        spacy_tokens = self.get_spacy_tokens()
+    def tokenize(self, input):
+        nlp_tokens = nlp(input)
 
-        print()
-        for spacy_token in spacy_tokens:
-            print(
-                f"{spacy_token.representation:<{15}}{spacy_token.lemma:<{15}}{spacy_token.start:<{10}}{spacy_token.end:<{10}}"
-            )
+        tokens = []
 
-        # openai_tokens = self.get_openai_tokens(spacy_tokens)
-
-        # self.create_tokens(spacy_tokens, openai_tokens)
-
-    def __repr__(self):
-        indent = 5
-        column_width = 20
-
-        row_labels = ["representation", "source", "target", "pos", "gender", "number"]
-
-        header_line = f"\n{'':<{indent}}{'':<{column_width}}{'spacy':<{column_width}}{'openai':<{column_width}}{'merged'}"
-
-        repr_str = header_line
-
-        for i, token in enumerate(self):
-            token_repr = token.__repr__(
-                number=i + 1,
-                indent=indent,
-                column_width=column_width,
-                row_labels=row_labels,
-            )
-            repr_str += "\n\n" + token_repr
-
-        return repr_str
-
-    def get_spacy_tokens(self):
-        source = self.text.source
-
-        parsed_objects = nlp(source)
-
-        spacy_tokens = []
-
-        for parsed_object in parsed_objects:
-            if parsed_object.pos_ == "PUNCT":
+        for nlp_token in nlp_tokens:
+            if nlp_token.pos_ == "PUNCT":
                 continue
 
-            spacy_token = SpacyToken(
-                parsed_object.text,
-                parsed_object.lemma_,
-                parsed_object.idx,
-                parsed_object.idx + len(parsed_object.text),
+            token = Token(
+                nlp_token.text,
+                nlp_token.lemma_,
+                nlp_token.idx,
+                nlp_token.idx + len(nlp_token.text),
             )
 
-            spacy_tokens.append(spacy_token)
+            tokens.append(token)
 
-        return spacy_tokens
-
-    def get_openai_tokens(self, spacy_tokens):
-        request_string = self.text.source
-
-        request_tokens = []
-
-        for spacy_token in spacy_tokens:
-            request_token = {
-                "representation": spacy_token.representation,
-                "source": spacy_token.source,
-                "pos": spacy_token.pos,
-                "gender": spacy_token.gender,
-                "number": spacy_token.number,
-            }
-            request_tokens.append(request_token)
-
-        request = {
-            "string": request_string,
-            "tokens": request_tokens,
-        }
-
-        response_objects = self.session.openai.get_tokens(request)
-
-        openai_tokens = []
-
-        for response_object in response_objects:
-            openai_token = OpenAiToken(response_object)
-            openai_tokens.append(openai_token)
-
-        return openai_tokens
-
-    def create_tokens(self, spacy_tokens, openai_tokens):
-        if len(spacy_tokens) != len(openai_tokens):
-            raise Exception("spacy_tokens is not the same length as openai_tokens")
-
-        for i, spacy_token in enumerate(spacy_tokens):
-            token = Token(self.text, spacy_token, openai_tokens[i])
-            self.append(token)
-
-        return self
+        return tokens
